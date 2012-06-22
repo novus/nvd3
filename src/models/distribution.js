2f10fbd8871ee4522cec05e41556cd2ad7c008dc
Bob Monteverde <bobmonteverde@gmail.com>
2012-06-22 18:56:30 -0400

    some progress on the abstracted distribution

--- src/models/distribution.js
+++ src/models/distribution.js
@@ -3,12 +3,12 @@ nv.models.sparkline = function() {
   var margin = {top: 0, right: 0, bottom: 0, left: 0},
       length = 400,
       size = 8,
-      getData = function(d) { return d },
       axis = 'x', // 'x' or 'y'... horizontal or vertical
+      getData = function(d) { return d[axis] },  // defaults d.x or d.y
       color = d3.scale.category20().range(),
       domain;
 
-  var scale = d3.scale.linear(),
+  var scale = d3.scale.linear();
 
   function chart(selection) {
     selection.each(function(data) {
@@ -19,9 +19,8 @@ nv.models.sparkline = function() {
       scale0 = scale0 || scale;
 
       scale
-          .domain(domain || d3.extent(data, getData ))
+          .domain(domain || d3.extent(data, getData))
           .range([0, availableLength]);
-
 
 
       var wrap = d3.select(this).selectAll('g.distribution').data([data]);
@@ -30,24 +29,29 @@ nv.models.sparkline = function() {
       gEnter
           .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
+      var g = wrap.select('g');
 
-      //.style('stroke', function(d,i) { return d.color || color[i * color.length] });
+      var distWrap = g.selectAll('g.dist')
+          .data(function(d) { return d }, function(d) { return d.key });
 
+      distWrap.enter().append('g')
+          .attr('class', function(d,i) { return 'dist series-' + i });
+      distWrap
+        .style('stroke', function(d,i) { return color.filter(function(d,i) { return data[i] && !data[i].disabled })[i % color.length] });
 
       var dist = distWrap.selectAll('line.dist' + axis)
-            .data(function(d) { return d.values })
+          .data(function(d) { return d.values })
       dist.enter().append('line')
           .attr(axis + '1', function(d,i) { return scale0(getData(d,i)) })
           .attr(axis + '2', function(d,i) { return scale0(getData(d,i)) })
-      //d3.transition(distX.exit())
       d3.transition(distWrap.exit().selectAll('line.dist' + axis))
           .attr(axis + '1', function(d,i) { return scale(getData(d,i)) })
           .attr(axis + '2', function(d,i) { return scale(getData(d,i)) })
           .remove();
       dist
           .attr('class', function(d,i) { return 'dist' + axis + ' dist-' + i })
-          .attr(naxis + '1', y.range()[0])
-          .attr(naxis + '2', y.range()[0] + size);
+          .attr(naxis + '1', 0)
+          .attr(naxis + '2', size);
       d3.transition(dist)
           .attr(axis + '1', function(d,i) { return scale(getData(d,i)) })
           .attr(axis + '2', function(d,i) { return scale(getData(d,i)) })
@@ -67,15 +71,15 @@ nv.models.sparkline = function() {
     return chart;
   };
 
-  chart.width = function(_) {
-    if (!arguments.length) return width;
-    width = _;
+  chart.length = function(_) {
+    if (!arguments.length) return length;
+    length = _;
     return chart;
   };
 
-  chart.height = function(_) {
-    if (!arguments.length) return height;
-    height = _;
+  chart.size = function(_) {
+    if (!arguments.length) return size;
+    size = _;
     return chart;
   };
 
