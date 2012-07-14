d737029bd2cd33d6bb7cf53288db4476f4b409bf
Itay Neeman <itay@neeman.net>
2012-07-13 19:08:21 -0700

    Fix incorrect date in tooltip of line+bar chart (#23)
    In a line+bar chart, the date was being set incorrectly due to the fact
    we did not pass in the point index to the x-axis computation.
    
    This fix passes it in, so now the tooltip can be calculated correctly.

--- src/models/linePlusBarChart.js
+++ src/models/linePlusBarChart.js
@@ -28,7 +28,7 @@ nv.models.linePlusBarChart = function() {
   var showTooltip = function(e, offsetElement) {
     var left = e.pos[0] + ( offsetElement.offsetLeft || 0 ),
         top = e.pos[1] + ( offsetElement.offsetTop || 0),
-        x = xAxis.tickFormat()(lines.x()(e.point)),
+        x = xAxis.tickFormat()(lines.x()(e.point, e.pointIndex)),
         y = yAxis1.tickFormat()(lines.y()(e.point)),
         content = tooltip(e.series.key, x, y, e, chart);
 
