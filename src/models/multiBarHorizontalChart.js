6534cb622f2d994e51da498589ad60aa4432c8cf
Bob Monteverde <bobmonteverde@gmail.com>
2012-07-30 14:51:17 -0400

    making tooltip public on multiBarHortizontalChart model

--- src/models/multiBarHorizontalChart.js
+++ src/models/multiBarHorizontalChart.js
@@ -276,6 +276,12 @@ nv.models.multiBarHorizontalChart = function() {
     return chart;
   };
 
+  chart.tooltip = function(_) {
+    if (!arguments.length) return tooltip;
+    tooltip = _;
+    return chart;
+  };
+
   chart.tooltips = function(_) {
     if (!arguments.length) return tooltips;
     tooltips = _;
