4f6e678def1a644572a0756b5480df6ff14b9552
Bob Monteverde <bobmonteverde@gmail.com>
2012-08-17 18:58:15 -0400

    Issue #68 updated distribution rug model to standard format

--- nv.d3.js
+++ nv.d3.js
@@ -2368,48 +2368,68 @@ nv.models.discreteBarChart = function() {
 }
 
 nv.models.distribution = function() {
-  var margin = {top: 0, right: 0, bottom: 0, left: 0},
-      width = 400, //technically width or height depending on x or y....
-      size = 8,
-      axis = 'x', // 'x' or 'y'... horizontal or vertical
-      getData = function(d) { return d[axis] },  // defaults d.x or d.y
-      color = nv.utils.defaultColor(),
-      domain;
 
-  var scale = d3.scale.linear(),
-      scale0;
+  //============================================================
+  // Public Variables with Default Settings
+  //------------------------------------------------------------
+
+  var margin = {top: 0, right: 0, bottom: 0, left: 0}
+    , width = 400 //technically width or height depending on x or y....
+    , size = 8
+    , axis = 'x' // 'x' or 'y'... horizontal or vertical
+    , getData = function(d) { return d[axis] }  // defaults d.x or d.y
+    , color = nv.utils.defaultColor()
+    , scale = d3.scale.linear()
+    , domain
+    ;
+
+  //============================================================
+
+
+  //============================================================
+  // Private Variables
+  //------------------------------------------------------------
+
+  var scale0;
+
+  //============================================================
+
 
   function chart(selection) {
     selection.each(function(data) {
       var availableLength = width - (axis === 'x' ? margin.left + margin.right : margin.top + margin.bottom),
-          naxis = axis == 'x' ? 'y' : 'x';
+          naxis = axis == 'x' ? 'y' : 'x',
+          container = d3.select(this);
 
 
-      //store old scales if they exist
+      //------------------------------------------------------------
+      // Setup Scales
+
       scale0 = scale0 || scale;
 
-/*
-      scale
-          .domain(domain || d3.extent(data, getData))
-          .range(axis == 'x' ? [0, availableLength] : [availableLength,0]);
-*/
+      //------------------------------------------------------------
 
 
-      var wrap = d3.select(this).selectAll('g.nv-distribution').data([data]);
+      //------------------------------------------------------------
+      // Setup containers and skeleton of chart
+
+      var wrap = container.selectAll('g.nv-distribution').data([data]);
       var wrapEnter = wrap.enter().append('g').attr('class', 'nvd3 nv-distribution');
       var gEnter = wrapEnter.append('g');
       var g = wrap.select('g');
 
       wrap.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
+      //------------------------------------------------------------
+
+
       var distWrap = g.selectAll('g.nv-dist')
           .data(function(d) { return d }, function(d) { return d.key });
 
-      distWrap.enter().append('g')
+      distWrap.enter().append('g');
       distWrap
           .attr('class', function(d,i) { return 'nv-dist nv-series-' + i })
           .style('stroke', function(d,i) { return color(d, i) });
-          //.style('stroke', function(d,i) { return color.filter(function(d,i) { return data[i] && !data[i].disabled })[i % color.length] });
 
       var dist = distWrap.selectAll('line.nv-dist' + axis)
           .data(function(d) { return d.values })
@@ -2422,12 +2442,10 @@ nv.models.distribution = function() {
           .style('stroke-opacity', 0)
           .remove();
       dist
-      //distWrap.selectAll('line.dist' + axis)
           .attr('class', function(d,i) { return 'nv-dist' + axis + ' nv-dist' + axis + '-' + i })
           .attr(naxis + '1', 0)
           .attr(naxis + '2', size);
       d3.transition(dist)
-      //d3.transition(distWrap.selectAll('line.dist' + axis))
           .attr(axis + '1', function(d,i) { return scale(getData(d,i)) })
           .attr(axis + '2', function(d,i) { return scale(getData(d,i)) })
 
@@ -2439,6 +2457,10 @@ nv.models.distribution = function() {
     return chart;
   }
 
+
+  //============================================================
+  // Expose Public Variables
+  //------------------------------------------------------------
 
   chart.margin = function(_) {
     if (!arguments.length) return margin;
@@ -2481,6 +2503,9 @@ nv.models.distribution = function() {
     color = nv.utils.getColor(_);
     return chart;
   };
+
+  //============================================================
+
 
   return chart;
 }
--- src/models/distribution.js
+++ src/models/distribution.js
@@ -1,47 +1,67 @@
 
 nv.models.distribution = function() {
-  var margin = {top: 0, right: 0, bottom: 0, left: 0},
-      width = 400, //technically width or height depending on x or y....
-      size = 8,
-      axis = 'x', // 'x' or 'y'... horizontal or vertical
-      getData = function(d) { return d[axis] },  // defaults d.x or d.y
-      color = nv.utils.defaultColor(),
-      domain;
 
-  var scale = d3.scale.linear(),
-      scale0;
+  //============================================================
+  // Public Variables with Default Settings
+  //------------------------------------------------------------
+
+  var margin = {top: 0, right: 0, bottom: 0, left: 0}
+    , width = 400 //technically width or height depending on x or y....
+    , size = 8
+    , axis = 'x' // 'x' or 'y'... horizontal or vertical
+    , getData = function(d) { return d[axis] }  // defaults d.x or d.y
+    , color = nv.utils.defaultColor()
+    , scale = d3.scale.linear()
+    , domain
+    ;
+
+  //============================================================
+
+
+  //============================================================
+  // Private Variables
+  //------------------------------------------------------------
+
+  var scale0;
+
+  //============================================================
+
 
   function chart(selection) {
     selection.each(function(data) {
       var availableLength = width - (axis === 'x' ? margin.left + margin.right : margin.top + margin.bottom),
-          naxis = axis == 'x' ? 'y' : 'x';
+          naxis = axis == 'x' ? 'y' : 'x',
+          container = d3.select(this);
 
 
-      //store old scales if they exist
+      //------------------------------------------------------------
+      // Setup Scales
+
       scale0 = scale0 || scale;
 
-/*
-      scale
-          .domain(domain || d3.extent(data, getData))
-          .range(axis == 'x' ? [0, availableLength] : [availableLength,0]);
-*/
+      //------------------------------------------------------------
 
 
-      var wrap = d3.select(this).selectAll('g.nv-distribution').data([data]);
+      //------------------------------------------------------------
+      // Setup containers and skeleton of chart
+
+      var wrap = container.selectAll('g.nv-distribution').data([data]);
       var wrapEnter = wrap.enter().append('g').attr('class', 'nvd3 nv-distribution');
       var gEnter = wrapEnter.append('g');
       var g = wrap.select('g');
 
       wrap.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
+      //------------------------------------------------------------
+
+
       var distWrap = g.selectAll('g.nv-dist')
           .data(function(d) { return d }, function(d) { return d.key });
 
-      distWrap.enter().append('g')
+      distWrap.enter().append('g');
       distWrap
           .attr('class', function(d,i) { return 'nv-dist nv-series-' + i })
           .style('stroke', function(d,i) { return color(d, i) });
-          //.style('stroke', function(d,i) { return color.filter(function(d,i) { return data[i] && !data[i].disabled })[i % color.length] });
 
       var dist = distWrap.selectAll('line.nv-dist' + axis)
           .data(function(d) { return d.values })
@@ -54,12 +74,10 @@ nv.models.distribution = function() {
           .style('stroke-opacity', 0)
           .remove();
       dist
-      //distWrap.selectAll('line.dist' + axis)
           .attr('class', function(d,i) { return 'nv-dist' + axis + ' nv-dist' + axis + '-' + i })
           .attr(naxis + '1', 0)
           .attr(naxis + '2', size);
       d3.transition(dist)
-      //d3.transition(distWrap.selectAll('line.dist' + axis))
           .attr(axis + '1', function(d,i) { return scale(getData(d,i)) })
           .attr(axis + '2', function(d,i) { return scale(getData(d,i)) })
 
@@ -71,6 +89,10 @@ nv.models.distribution = function() {
     return chart;
   }
 
+
+  //============================================================
+  // Expose Public Variables
+  //------------------------------------------------------------
 
   chart.margin = function(_) {
     if (!arguments.length) return margin;
@@ -113,6 +135,9 @@ nv.models.distribution = function() {
     color = nv.utils.getColor(_);
     return chart;
   };
+
+  //============================================================
+
 
   return chart;
 }
