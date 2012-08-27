7b58d8fd9ad1c6f69ee8f00633c69d4d1261609a
Bob Monteverde <bobmonteverde@gmail.com>
2012-08-27 13:30:17 -0400

    Fixed Issues #161 in the pie

--- src/models/pie.js
+++ src/models/pie.js
@@ -229,7 +229,10 @@ nv.models.pie = function() {
 
   chart.margin = function(_) {
     if (!arguments.length) return margin;
-    margin = _;
+    margin.top    = typeof _.top    != 'undefined' ? _.top    : margin.top;
+    margin.right  = typeof _.right  != 'undefined' ? _.right  : margin.right;
+    margin.bottom = typeof _.bottom != 'undefined' ? _.bottom : margin.bottom;
+    margin.left   = typeof _.left   != 'undefined' ? _.left   : margin.left;
     return chart;
   };
 
