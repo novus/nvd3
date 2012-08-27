628936448f2a074c24e4dbb122ff18a45033054d
Bob Monteverde <bobmonteverde@gmail.com>
2012-08-27 11:36:13 -0400

    Minifying Pull Request #173

--- nv.d3.js
+++ nv.d3.js
@@ -2879,7 +2879,6 @@ nv.models.indentedTree = function() {
 
   return chart;
 }
-
 nv.models.legend = function() {
 
   //============================================================
@@ -2932,17 +2931,18 @@ nv.models.legend = function() {
             dispatch.legendDblclick(d,i);
           });
       seriesEnter.append('circle')
-          .style('fill', function(d,i) { return d.color || color(d,i)})
-          .style('stroke', function(d,i) { return d.color || color(d, i) })
           .style('stroke-width', 2)
           .attr('r', 5);
       seriesEnter.append('text')
-          .text(getKey)
           .attr('text-anchor', 'start')
           .attr('dy', '.32em')
           .attr('dx', '8');
       series.classed('disabled', function(d) { return d.disabled });
       series.exit().remove();
+      series.select('circle')
+          .style('fill', function(d,i) { return d.color || color(d,i)})
+          .style('stroke', function(d,i) { return d.color || color(d, i) });
+      series.select('text').text(getKey);
 
 
       //TODO: implement fixed-width and max-width options (max-width is especially useful with the align option)
