6ac9238e59789b48a989d631c1c7343d1dd9fca1
Bob Monteverde <bobmonteverde@gmail.com>
2012-09-13 17:27:39 -0400

    Fixed issue #198 removing empty paths

--- nv.d3.js
+++ nv.d3.js
@@ -3350,7 +3350,7 @@ nv.models.line = function() {
                 //.y1(function(d,i) { return y0(0) }) //assuming 0 is within y domain.. may need to tweak this
                 .apply(this, [d.values])
           });
-      areaPaths.exit().remove(); // attempting to fix issue #198
+      groups.selectAll('path:not(.nv-area)').remove(); // attempting to fix issue #198
       d3.transition(groups.exit().selectAll('path.nv-area'))
           .attr('d', function(d) {
             return d3.svg.area()
@@ -3379,7 +3379,7 @@ nv.models.line = function() {
       var linePaths = groups.selectAll('path.nv-line')
           .data(function(d) { return [d.values] });
       linePaths.enter().append('path')
-          .attr('class', function(d) { return 'nv-line' })
+          .attr('class', 'nv-line')
           .attr('d',
             d3.svg.line()
               .interpolate(interpolate)
@@ -3387,6 +3387,7 @@ nv.models.line = function() {
               .x(function(d,i) { return x0(getX(d,i)) })
               .y(function(d,i) { return y0(getY(d,i)) })
           );
+      groups.selectAll('path:not(.nv-line)').remove(); // attempting to fix issue #198
       d3.transition(groups.exit().selectAll('path.nv-line'))
           .attr('d',
             d3.svg.line()
