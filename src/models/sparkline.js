b9d8000f0eea188e145baf85f8ee883c0524815f
Ricardo <rleon@novus.com>
2012-11-02 12:04:02 -0400

    Fix for #301: now it will display point only for the first min value, the last max value, and the current value.

--- src/models/sparkline.js
+++ src/models/sparkline.js
@@ -68,11 +68,20 @@ nv.models.sparkline = function() {
       // TODO: Add CURRENT data point (Need Min, Mac, Current / Most recent)
       var points = wrap.selectAll('circle.nv-point')
           .data(function(data) {
-            return data.map(function(d,i) {
-              if (y.domain().indexOf(getY(d,i)) != -1 || getX(d,i) == x.domain()[1]) d.pointIndex = i;
-              return d;
-            })
-            .filter(function(d) { return typeof d.pointIndex != 'undefined' })
+              var yValues = data.map(function(d, i) { return getY(d,i); });
+              function pointIndex(index) {
+                  if (index != -1) {
+	              var result = data[index];
+                      result.pointIndex = index;
+                      return result;
+                  } else {
+                      return null;
+                  }
+              }
+              var maxPoint = pointIndex(yValues.lastIndexOf(y.domain()[1])),
+                  minPoint = pointIndex(yValues.indexOf(y.domain()[0])),
+                  currentPoint = pointIndex(yValues.length - 1);
+              return [minPoint, maxPoint, currentPoint].filter(function (d) {return d != null;});
           });
       points.enter().append('circle');
       points.exit().remove();
