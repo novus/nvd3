53687e145426e12320747c99ddba653eaff5bf4c
Tyler Wolf <twolf@twolf-ubuntu.Novus.local>
2012-11-07 11:27:55 -0500

    reverted the lineChart right margin change. My mistake

--- nv.d3.js
+++ nv.d3.js
@@ -3640,7 +3640,7 @@ nv.models.lineChart = function() {
     ;
 
 //set margin.right to 23 to fit dates on the x-axis within the chart
-  var margin = {top: 30, right: 23, bottom: 50, left: 60}
+  var margin = {top: 30, right: 20, bottom: 50, left: 60}
     , color = nv.utils.defaultColor()
     , width = null
     , height = null
