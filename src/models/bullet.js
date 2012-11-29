51d9d091c008b7060c7614bab8bc8de908acd8f3
Bob Monteverde <bob@montev.com>
2012-11-29 00:03:11 -0500

    Implemented forceX to bulletChart

--- src/models/bullet.js
+++ src/models/bullet.js
@@ -42,7 +42,7 @@ nv.models.bullet = function() {
 
       // Compute the new x-scale.
       var x1 = d3.scale.linear()
-          .domain([0, Math.max(rangez[0], markerz[0], measurez[0])])  // TODO: need to allow forceX and forceY, and xDomain, yDomain
+          .domain([0, d3.max(d3.merge([forceX, rangez]))])  // TODO: need to allow forceX and forceY, and xDomain, yDomain
           .range(reverse ? [availableWidth, 0] : [0, availableWidth]);
 
       // Retrieve the old x-scale, if this is an update.
