93a26c53c9a1000c54617feb04f8e6b89877d6ac
Robin Hu <robinfhu@gmail.com>
2013-07-05 18:08:45 -0400

    Fixing issue in scatterPlusLineChart where tooltips don't activate on chart initial load.

--- src/models/scatterPlusLineChart.js
+++ src/models/scatterPlusLineChart.js
@@ -167,7 +167,7 @@ nv.models.scatterPlusLineChart = function() {
       var g = wrap.select('g')
 
       // background for pointer events
-      gEnter.append('rect').attr('class', 'nvd3 nv-background')
+      gEnter.append('rect').attr('class', 'nvd3 nv-background').style("pointer-events","none");
 
       gEnter.append('g').attr('class', 'nv-x nv-axis');
       gEnter.append('g').attr('class', 'nv-y nv-axis');
@@ -330,7 +330,8 @@ nv.models.scatterPlusLineChart = function() {
       if (d3.fisheye) {
         g.select('.nv-background')
             .attr('width', availableWidth)
-            .attr('height', availableHeight);
+            .attr('height', availableHeight)
+            ;
 
         g.select('.nv-background').on('mousemove', updateFisheye);
         g.select('.nv-background').on('click', function() { pauseFisheye = !pauseFisheye;});
