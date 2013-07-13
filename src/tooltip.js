94efcbad66fa7860ee124c53ac172bf5d40d5258
Robin Hu <robinfhu@gmail.com>
2013-07-12 23:24:12 -0400

    Brought back old tooltip transitions and box shadow... but only if you supply the 'with-transitions' CSS class.
    Also changed the nv-point.hover dot to a smaller, darker stroke.

--- src/tooltip.js
+++ src/tooltip.js
@@ -120,7 +120,7 @@ window.nv.tooltip.* also has various helper methods.
         
 
             container.node().innerHTML = newContent;
-            container.style("top",0).style("left",0);
+            container.style("top",0).style("left",0).style("opacity",0);
             return container.node();
         }
 
