b102af9edda4bd42f9f49a6b378b01e63e2d963a
Robin Hu <robinfhu@gmail.com>
2013-08-17 16:56:04 -0400

    Removed a line from multibar chart that was referencing undefined variable 'singlePoint'.

--- nv.d3.js
+++ nv.d3.js
@@ -7512,7 +7512,6 @@ nv.models.multiBar = function() {
           .range(yRange || [availableHeight, 0]);
 
       // If scale's domain don't have a range, slightly adjust to make one... so a chart can show a single data point
-      if (x.domain()[0] === x.domain()[1] || y.domain()[0] === y.domain()[1]) singlePoint = true;
       if (x.domain()[0] === x.domain()[1])
         x.domain()[0] ?
             x.domain([x.domain()[0] - x.domain()[0] * 0.01, x.domain()[1] + x.domain()[1] * 0.01])
