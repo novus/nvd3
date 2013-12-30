b8d6f750abc3f62fadb2fd685730a35d126ff8eb
Robin Hu <robinfhu@gmail.com>
2013-12-30 13:09:02 -0500

    Added defensive code to the height calculation for <rect> elements, in lineChart and scatter.js

--- src/models/scatter.js
+++ src/models/scatter.js
@@ -146,7 +146,7 @@ nv.models.scatter = function() {
 
       wrap.select('#nv-edge-clip-' + id + ' rect')
           .attr('width', availableWidth)
-          .attr('height', availableHeight);
+          .attr('height', (availableHeight > 0) ? availableHeight : 0);
 
       g   .attr('clip-path', clipEdge ? 'url(#nv-edge-clip-' + id + ')' : '');
 
