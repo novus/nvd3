8c91d3a1d0bdbb81e279f8ac0d55ff081b8167df
Alexander Kasheverov <alexander.kasheverov@dataart.com>
2014-04-16 14:00:23 +0400

    refactoring

--- src/models/sparkline/sparkline.js
+++ src/models/sparkline/sparkline.js
@@ -1,182 +1,124 @@
+var SparklinePrivates = {
+    animate : true
+    , xScale : d3.scale.linear()
+    , yScale : d3.scale.linear()
+    , xDomain : null
+    , yDomain : null
+    , xRange : null
+    , yRange : null
+    , _color: nv.utils.getColor(['#000000'])
+};
 
-nv.models.sparkline = function() {
-  "use strict";
-  //============================================================
-  // Public Variables with Default Settings
-  //------------------------------------------------------------
-
-  var Layer = new Layer({
+/**
+ * A Sparkline
+ */
+function Sparkline(options){
+    options = nv.utils.extend({}, options, SparklinePrivates, {
         margin: {top: 2, right: 0, bottom: 2, left: 0}
         , width : 400
         , height : 32
         , chartClass: 'sparkline'
-      })
-    , animate = true
-    , x = d3.scale.linear()
-    , y = d3.scale.linear()
-    , getX = function(d) { return d.x }
-    , getY = function(d) { return d.y }
-    , color = nv.utils.getColor(['#000'])
-    , xDomain
-    , yDomain
-    , xRange
-    , yRange
-    ;
-
-  //============================================================
-
-
-  function chart(selection) {
-    selection.each(function(data) {
-
-      Layer.setRoot(this);
-      Layer.wrapChart(data);
-
-      var availableWidth = Layer.available.width,
-          availableHeight = Layer.available.height;
-
-      //------------------------------------------------------------
-      // Setup Scales
-
-      x.domain(xDomain || d3.extent(data, getX ))
-        .range(xRange || [0, availableWidth]);
-
-      y.domain(yDomain || d3.extent(data, getY ))
-        .range(yRange || [availableHeight, 0]);
-
-      //------------------------------------------------------------
-
-      var paths = Layer.wrap.selectAll('path')
-          .data(function(d) { return [d] });
-      paths.enter().append('path');
-      paths.exit().remove();
-      paths
-        .style('stroke', function(d,i) { return d.color || color(d, i) })
-        .attr('d', d3.svg.line()
-          .x(function(d,i) { return x(getX(d,i)) })
-          .y(function(d,i) { return y(getY(d,i)) })
-        );
-
-      // TODO: Add CURRENT data point (Need Min, Mac, Current / Most recent)
-      var points = Layer.wrap.selectAll('circle.nv-point')
-        .data(function(data) {
-          var yValues = data.map(function(d, i) { return getY(d,i); });
-          function pointIndex(index) {
-            if (index != -1) {
-	        var result = data[index];
-                result.pointIndex = index;
-                return result;
-            } else
-                return null;
-          }
-          var maxPoint = pointIndex(yValues.lastIndexOf(y.domain()[1])),
-              minPoint = pointIndex(yValues.indexOf(y.domain()[0])),
-              currentPoint = pointIndex(yValues.length - 1);
-          return [minPoint, maxPoint, currentPoint].filter(function (d) {return d != null;});
-        });
-      points.enter().append('circle');
-      points.exit().remove();
-      points
-        .attr('cx', function(d) { return x(getX(d,d.pointIndex)) })
-        .attr('cy', function(d) { return y(getY(d,d.pointIndex)) })
-        .attr('r', 2)
-        .attr('class', function(d) {
-            return getX(d, d.pointIndex) == x.domain()[1] ? 'nv-point nv-currentValue' :
-                   getY(d, d.pointIndex) == y.domain()[0] ? 'nv-point nv-minValue' : 'nv-point nv-maxValue'
-          });
     });
 
+    Layer.call(this, options, []);
+}
+
+nv.utils.create(Sparkline, Layer, SparklinePrivates);
+
+/**
+ * @override Layer::wrapper
+ */
+Sparkline.prototype.wrapper = function(data){
+    Layer.prototype.wrapper.call(this, data, []);
+};
+
+/**
+ * @override Layer::draw
+ */
+Sparkline.prototype.draw = function(data){
+
+    var that = this
+        , availableWidth = this.available.width
+        , availableHeight = this.available.height;
+
+    //------------------------------------------------------------
+    // Setup Scales
+
+    this.xScale().domain(this.xDomain() || d3.extent(data, this.x() ))
+        .range(this.xRange() || [0, availableWidth]);
+
+    this.yScale().domain(this.yDomain() || d3.extent(data, this.y() ))
+        .range(this.yRange() || [availableHeight, 0]);
+
+    //------------------------------------------------------------
+
+    var paths = this.wrap.selectAll('path')
+        .data(function(d) { return [d] });
+    paths.enter().append('path');
+    paths.exit().remove();
+    paths
+        .style('stroke', function(d,i) { return d.color || that.color()(d, i) })
+        .attr('d', d3.svg.line()
+            .x(function(d,i) { return that.xScale()(that.x()(d,i)) })
+            .y(function(d,i) { return that.yScale()(that.y()(d,i)) })
+        );
+
+    // TODO: Add CURRENT data point (Need Min, Mac, Current / Most recent)
+    var points = this.wrap.selectAll('circle.nv-point')
+        .data(function(data) {
+            var yValues = data.map(function(d, i) { return that.y()(d,i); });
+            function pointIndex(index) {
+                if (index != -1) {
+                    var result = data[index];
+                    result.pointIndex = index;
+                    return result;
+                } else
+                    return null;
+            }
+            var maxPoint = pointIndex(yValues.lastIndexOf(that.yScale().domain()[1])),
+                minPoint = pointIndex(yValues.indexOf(that.yScale().domain()[0])),
+                currentPoint = pointIndex(yValues.length - 1);
+            return [minPoint, maxPoint, currentPoint].filter(function (d) {return d != null;});
+        });
+    points.enter().append('circle');
+    points.exit().remove();
+    points
+        .attr('cx', function(d) { return that.xScale()(that.x()(d,d.pointIndex)) })
+        .attr('cy', function(d) { return that.yScale()(that.y()(d,d.pointIndex)) })
+        .attr('r', 2)
+        .attr('class', function(d) {
+            return that.x()(d, d.pointIndex) == that.xScale().domain()[1] ? 'nv-point nv-currentValue' :
+                that.y()(d, d.pointIndex) == that.yScale().domain()[0] ? 'nv-point nv-minValue' : 'nv-point nv-maxValue'
+        });
+};
+
+Sparkline.prototype.color = function(_) {
+    if (!arguments.length) return this._color();
+    this._color( nv.utils.getColor(_) );
+    return this;
+};
+
+/**
+ * The sparkline model returns a function wrapping an instance of a Sparkline.
+ */
+nv.models.sparkline = function () {
+    "use strict";
+
+    var sparkline = new Sparkline();
+
+    function chart(selection) {
+        sparkline.render(selection);
+        return chart;
+    }
+
+    chart.dispatch = sparkline.dispatch;
+
+    chart.options = nv.utils.optionsFunc.bind(chart);
+
+    nv.utils.rebindp(chart, sparkline, Sparkline.prototype,
+        'margin', 'width', 'height', 'x', 'y', 'xScale', 'yScale', 'xDomain', 'yDomain', 'xRange', 'yRange',
+        'animate', 'color'
+    );
+
     return chart;
-  }
-
-
-  //============================================================
-  // Expose Public Variables
-  //------------------------------------------------------------
-  chart.options = nv.utils.optionsFunc.bind(chart);
-
-  chart.margin = function(_) {
-    if (!arguments.length) return Layer.margin;
-    Layer.margin.top    = typeof _.top    != 'undefined' ? _.top    : Layer.margin.top;
-    Layer.margin.right  = typeof _.right  != 'undefined' ? _.right  : Layer.margin.right;
-    Layer.margin.bottom = typeof _.bottom != 'undefined' ? _.bottom : Layer.margin.bottom;
-    Layer.margin.left   = typeof _.left   != 'undefined' ? _.left   : Layer.margin.left;
-    return chart;
-  };
-
-  chart.width = function(_) {
-    if (!arguments.length) return Layer.options.size.width;
-    Layer.options.size.width = _;
-    return chart;
-  };
-
-  chart.height = function(_) {
-    if (!arguments.length) return Layer.options.size.height;
-    Layer.options.size.height = _;
-    return chart;
-  };
-
-  chart.x = function(_) {
-    if (!arguments.length) return getX;
-    getX = d3.functor(_);
-    return chart;
-  };
-
-  chart.y = function(_) {
-    if (!arguments.length) return getY;
-    getY = d3.functor(_);
-    return chart;
-  };
-
-  chart.xScale = function(_) {
-    if (!arguments.length) return x;
-    x = _;
-    return chart;
-  };
-
-  chart.yScale = function(_) {
-    if (!arguments.length) return y;
-    y = _;
-    return chart;
-  };
-
-  chart.xDomain = function(_) {
-    if (!arguments.length) return xDomain;
-    xDomain = _;
-    return chart;
-  };
-
-  chart.yDomain = function(_) {
-    if (!arguments.length) return yDomain;
-    yDomain = _;
-    return chart;
-  };
-
-  chart.xRange = function(_) {
-    if (!arguments.length) return xRange;
-    xRange = _;
-    return chart;
-  };
-
-  chart.yRange = function(_) {
-    if (!arguments.length) return yRange;
-    yRange = _;
-    return chart;
-  };
-
-  chart.animate = function(_) {
-    if (!arguments.length) return animate;
-    animate = _;
-    return chart;
-  };
-
-  chart.color = function(_) {
-    if (!arguments.length) return color;
-    color = nv.utils.getColor(_);
-    return chart;
-  };
-
-  //============================================================
-
-  return chart;
 };
