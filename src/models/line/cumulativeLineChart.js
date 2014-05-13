172af831a676eec8a855a9bdaefa391f993f81b2
Alexander Kasheverov <alexander.kasheverov@dataart.com>
2014-05-13 17:11:25 +0400

    refactor/2.0.0-dev/cumulativeLineChart

--- src/models/line/cumulativeLineChart.js
+++ src/models/line/cumulativeLineChart.js
@@ -16,6 +16,7 @@ var CumulativeLineChartPrivates = {
     , yScale : null
     , duration : 250
     , useInteractiveGuideline : false
+    , state: null
 };
 
 function CumulativeLineChart(options){
@@ -525,7 +526,28 @@ CumulativeLineChart.prototype.y = function(_){
 nv.models.cumulativeLineChart = function(){
     "use strict";
 
-    var cumulativeLineChart = new CumulativeLineChart();
+    var cumulativeLineChart = new CumulativeLineChart(),
+        api = [
+            'margin',
+            'width',
+            'height',
+            'color',
+            'rescaleY',
+            'showControls',
+            'useInteractiveGuideline',
+            'showLegend',
+            'showXAxis',
+            'showYAxis',
+            'tooltips',
+            'tooltipContent',
+            'state',
+            'defaultState',
+            'noData',
+            'average',
+            'transitionDuration',
+            'duration',
+            'noErrorCheck'
+        ];
 
     function chart(selection){
         cumulativeLineChart.render(selection);
@@ -540,17 +562,25 @@ nv.models.cumulativeLineChart = function(){
     chart.yAxis = cumulativeLineChart.yAxis;
 
     d3.rebind(chart, cumulativeLineChart.line,
-        'defined', 'isArea', 'size', 'xDomain', 'yDomain', 'xRange', 'yRange', 'forceX',
-        'forceY', 'interactive', 'clipEdge', 'clipVoronoi', 'useVoronoi',  'id'
+        'isArea',
+        'x',
+        'y',
+        'xDomain',
+        'yDomain',
+        'xRange',
+        'yRange',
+        'forceX',
+        'forceY',
+        'interactive',
+        'clipEdge',
+        'clipVoronoi',
+        'useVoronoi',
+        'id'
     );
 
     chart.options = nv.utils.optionsFunc.bind(chart);
 
-    nv.utils.rebindp(chart, cumulativeLineChart, CumulativeLineChart.prototype,
-        'margin', 'width', 'height', 'color', 'rescaleY', 'showControls', 'useInteractiveGuideline', 'showLegend',
-        'showXAxis', 'showYAxis', 'rightAlignYAxis', 'tooltips', 'tooltipContent', 'state', 'defaultState',
-        'noData', 'average', 'transitionDuration', 'duration', 'noErrorCheck', 'x', 'y'
-    );
+    nv.utils.rebindp(chart, cumulativeLineChart, CumulativeLineChart.prototype, api);
 
     return chart;
 };
