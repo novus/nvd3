5ab848d3ffa8ad7691e2736329c29bc5df3b45d6
Antoine Rousseau <antoine@rousseau.im>
2014-07-07 13:44:00 +0200

    add support for component.io

--- src/outro.js
+++ src/outro.js
@@ -1,1 +1,8 @@
+  if (typeof define === "function" && define.amd) {
+    define(nv);
+  } else if (typeof module === "object" && module.exports) {
+    module.exports = nv;
+  } else {
+    this.nv = nv;
+  }
 })();
