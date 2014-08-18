498a46f06a305cf80d65200829d368299a35cf8c
Leon Lin <ientin.lin@gmail.com>
2014-08-18 18:10:11 +0800

    add tooltip & its event ; main components of horizonBarChart.js

--- src/models/horizontalBarChart.js
+++ src/models/horizontalBarChart.js
@@ -10,9 +10,167 @@ nv.models.horizontalBarChart = function(){
 		yAxis = nv.models.axis(),
 		legend = nv.models.legend().height(30)
 		;
+
+	var margin = {top: 15, right: 20, bottom: 50, left: 60},
+		width = null,
+		height = null,
+		color = nv.utils.getColor(),
+		showXAxis = true,
+		showYAxis = true,
+		showLegend = true,
+		rightAlignYAxis = false,
+		tooltips = true,
+		tooltip = function(point, x, y, e, graph){
+			return '<h3>' + y + '</h3>' +
+					'<p>' + x + '</p>';
+		},
+		x,
+		y,
+		noData = "No Data Available",
+		dispatch = d3.dispatch('tooltipShow', 'tooltipHide', 'beforeUpdate'),
+		transitionDuration = 250,
+		state = {},
+		changed = true
+		;
+
+	xAxis
+	    .orient('bottom')
+		.tickFormat(d3.format(',.f'))
+    	;
+	yAxis
+		.orient((rightAlignYAxis) ? 'right' : 'left')
+	    .highlightZero(false)
+	    .showMaxMin(false)
+	    .tickFormat(function(d) { return d })
+    	;
+
+	/* private variables */
+	var showTooltip = function(e, offsetElement) {
+	    var left = e.pos[0] + ( offsetElement.offsetLeft || 0 ) + margin.left,
+	        top = e.pos[1] + ( offsetElement.offsetTop || 0) + 2 * margin.top,
+	        x = xAxis.tickFormat()(horizontalBar.x()(e.point, e.pointIndex)),
+	        y = yAxis.tickFormat()(horizontalBar.y()(e.point, e.pointIndex)),
+	        content = tooltip(e.point, x, y, e, chart);
+			nv.tooltip.show([left , top], content, e.value < 0 ? 'n' : 's', null, offsetElement);
+	};
 	/* ----------------  */
 
 	function chart(selection) {
+		selection.each(function(data) {
+			var container = d3.select(this),
+				that = this;
+
+			var availableWidth = (width  || parseInt(container.style('width')) || 960)
+							- margin.left - margin.right,
+          		availableHeight = (height || parseInt(container.style('height')) || 400)
+							- margin.top - margin.bottom;
+
+			chart.update = function() {
+				dispatch.beforeUpdate();
+				container.transition().duration(transitionDuration).call(chart);
+			};
+
+			chart.container = this;
+
+			/* no data exists */
+			var enoughLength = data.filter(function(d) { 
+				return d.values.length 
+			}).length;
+
+			if( !data || !data.length || !enoughLength ) {
+		        var noDataText = container.selectAll('.nv-noData').data([noData]);
+
+		        noDataText.enter().append('text')
+		          .attr('class', 'nvd3 nv-noData')
+		          .attr('dy', '-.7em')
+		          .style('text-anchor', 'middle');
+
+		        noDataText
+		          .attr('x', margin.left + availableWidth / 2)
+		          .attr('y', margin.top + availableHeight / 2)
+		          .text(function(d) { return d });
+
+		        return chart;
+			} else {
+				container.selectAll('.nv-noData').remove();
+			}
+
+			// setup scales
+			x = horizontalBar.xScale().clamp(true);
+			y= horizontalBar.yScale();
+
+			//Setup containers and skeleton of chart
+			var wrap = container.selectAll('g.nv-wrap.nv-horizontalBarWithAxes').data([data]);
+			var gEnter = wrap.enter().append('g').attr('class', 'nvd3 nv-wrap nv-horizontalBarWithAxes').append('g');
+			var defsEnter = gEnter.append('defs');
+			var g = wrap.select('g');
+			
+			gEnter.append('g').attr('class', 'nv-x nv-axis');
+			gEnter.append('g').attr('class', 'nv-y nv-axis')
+			    .append('g').attr('class', 'nv-zeroLine')
+			    .append('line');
+
+			gEnter.append('g').attr('class', 'nv-barsWrap');
+			gEnter.append('g').attr('class', 'nv-legendWrap');
+
+			g.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+			if (rightAlignYAxis) {
+			  g.select(".nv-y.nv-axis")
+			      .attr("transform", "translate(" + availableWidth + ",0)");
+			}
+
+			/* Drawing each components */
+
+
+			wrap.attr('transform', 'translate(0 ,' + margin.top + ')');
+			// Main Chart Components
+			horizontalBar
+				.width(availableWidth)
+				.height(availableHeight)
+		        .color(data[0].values
+		        	.map(function(d,i) {
+		          		return d.color || color(d, i);
+		        	})	
+		        	.filter(function(d,i) { 
+		        		return !data[0].values[i].disabled 
+		        	})
+		        );
+
+
+			/* disabled data */
+			var barData = {} ;
+			barData.key = data[0].key; 
+			barData.values = data[0].values.filter(function(d) {
+				return !d.disabled;
+			});
+
+			var barsWrap = g.select('.nv-barsWrap')
+				.datum([barData]);
+
+			barsWrap.transition().call(horizontalBar);
+
+			/* ----------------- */
+
+			defsEnter.append('clipPath')
+					.attr('id', 'nv-x-label-clip-' + horizontalBar.id())
+					.append('rect')
+					;
+			// g.select('#nv-x-label-clip-' + horizontalBar.id() + ' rect')
+			// .attr('width')
+			// .attr('height')
+			// .attr('x');
+
+
+			/* Event Handling / Dispatching */
+
+			dispatch.on('tooltipShow', function(e) {
+		        if (tooltips) {
+		        	showTooltip(e, that.parentNode);
+		        }
+			});
+
+		});
 
 		return chart;
 	}
@@ -48,6 +206,110 @@ nv.models.horizontalBarChart = function(){
 	d3.rebind(chart, horizontalBar, 'x', 'y', 'xDomain', 'yDomain', 'xRange', 'yRange', 'forceX', 'forceY', 'id', 'showValues', 'valueFormat');
 
 	chart.options = nv.utils.optionsFunc.bind(chart);
+  
+	chart.margin = function(_) {
+		if (!arguments.length) 
+			return margin;
+		margin.top    = typeof _.top    != 'undefined' ? _.top    : margin.top;
+		margin.right  = typeof _.right  != 'undefined' ? _.right  : margin.right;
+		margin.bottom = typeof _.bottom != 'undefined' ? _.bottom : margin.bottom;
+		margin.left   = typeof _.left   != 'undefined' ? _.left   : margin.left;
+		return chart;
+	};
+
+	chart.width = function(_) {
+		if (!arguments.length) 
+			return width;
+		width = _;
+		return chart;
+	};
+
+	chart.height = function(_) {
+		if (!arguments.length) 
+			return height;
+		height = _;
+		return chart;
+	};
+
+	chart.color = function(_) {
+		if (!arguments.length) 
+			return color;
+		color = nv.utils.getColor(_);
+		horizontalBar.color(color);
+		return chart;
+	};
+
+	chart.showXAxis = function(_) {
+		if (!arguments.length) 
+			return showXAxis;
+		showXAxis = _;
+		return chart;
+	};
+
+	chart.showYAxis = function(_) {
+		if (!arguments.length) 
+			return showYAxis;
+		showYAxis = _;
+		return chart;
+	};
+
+	chart.showLegend = function(_) {
+		if (!arguments.length) {
+			return showLegend;	
+		}
+		showLegend = _;
+		return chart;
+	};
+
+	chart.rightAlignYAxis = function(_) {
+		if(!arguments.length) 
+			return rightAlignYAxis;
+		rightAlignYAxis = _;
+		yAxis.orient( (_) ? 'right' : 'left');
+		return chart;
+	};
+
+	chart.tooltips = function(_) {
+		if (!arguments.length) 
+			return tooltips;
+		tooltips = _;
+		return chart;
+	};
+
+	chart.tooltipContent = function(_) {
+		if (!arguments.length) 
+			return tooltip;
+		tooltip = _;
+		return chart;
+	};
+
+  	chart.noData = function(_) {
+    	if (!arguments.length) 
+    		return noData;
+		noData = _;
+		return chart;
+	};
+
+	chart.transitionDuration = function(_) {
+	    if (!arguments.length) 
+	    	return transitionDuration;
+	    transitionDuration = _;
+	    return chart;
+	};
+
+   	chart.xAxis = function(_) {
+	    if (!arguments.length) 
+	    	return xAxis;
+	    xAxis = _;
+	    return chart;
+	};
+
+	chart.yAxis = function(_) {
+	    if (!arguments.length) 
+	    	return yAxis;
+	    yAxis = _;
+	    return chart;
+	};
 
 
 	return chart;
