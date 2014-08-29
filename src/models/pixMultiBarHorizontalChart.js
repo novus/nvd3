313b0088330c50c7ac6c76a64eca0694043e47b0
Leon Lin <ientin.lin@gmail.com>
2014-08-29 15:53:54 +0800

    rename pixStackBarHorizontalChart to pixMultiBarHorizontalChart

--- src/models/pixMultiBarHorizontalChart.js
+++ src/models/pixMultiBarHorizontalChart.js
@@ -1,5 +1,5 @@
 
-nv.models.pixStackBarHorizontalChart = function() {
+nv.models.pixMultiBarHorizontalChart = function() {
   "use strict";
   //============================================================
   // Public Variables with Default Settings
