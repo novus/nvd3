c35d522edb4bfec6ca2c8485a9ca8f377fa34cb9
Aaron Salvo <aaron.salvo@teamaol.com>
2014-10-20 11:19:08 -0400

    Various bug fixes.

--- src/models/sumoUtilization.js
+++ src/models/sumoUtilization.js
@@ -4,16 +4,21 @@ nv.models.sumoUtilization = function() {
   // Public Variables with Default Settings
   //------------------------------------------------------------
 
-  var sumoUtilization = this;
+  var sumoUtilization = this
+    , legend = nv.models.legend()
+    ;
 
   var margin = {top: 10, right: 10, bottom: 10, left: 10}
     , width  = 470
     , height = 65
     , id = Math.floor(Math.random() * 10000) // Create a semi-unique ID in case the user doesn't select one
-    , colors = ['#c07a78', '#c68a6f', '#cc9873', '#d8b973', '#ded575', '#89c793' ]
+    , gradientColors = ['#c07a78', '#c68a6f', '#cc9873', '#d8b973', '#ded575', '#89c793' ]
     , bands = [10, 20, 30, 40, 50]
     , tooltips = true
+    , showLegend = true
+    , getLabel = function(d) { return d.key }
     , dispatch = d3.dispatch('elementMouseover', 'tooltipShow', 'elementMouseout', 'tooltipHide')
+    , color = nv.utils.defaultColor(['#5787b7','#ff6600'])
     ;
 
   //============================================================
@@ -50,7 +55,7 @@ nv.models.sumoUtilization = function() {
 
       var threshold = d3.scale.threshold()
                         .domain(bands)
-                        .range(colors);
+                        .range(gradientColors);
 
       var x = d3.scale.linear()
                 .domain([0,100])
@@ -62,60 +67,84 @@ nv.models.sumoUtilization = function() {
       //------------------------------------------------------------
       // Setup containers and skeleton of chart
 
-      var wrap = container.selectAll('g.nv-wrap.nv-sumoutilization').data([data]);
-      var wrapEnter = wrap.enter().append('g').attr('class', 'nvd3 nv-wrap nv-sumoutilization');
-      var gEnter = wrapEnter.append('g');
+      var wrap = container.selectAll('g.nv-wrap.nv-gradient').data([data]);
+      var gEnter = wrap.enter().append('g')
+                       .attr('class', 'nvd3 nv-wrap nv-gradient')
+                       .append('g');
+      var g = wrap.select('g');
 
+      gEnter.append('g').attr('class','nv-gradientWrap');
+      gEnter.append('g').attr('class', 'nv-legendWrap');
+
+      //------------------------------------------------------------
+      // Legend
+
+      if (showLegend) {
+        legend
+          .width( availableWidth )
+          .key();
+
+        wrap.select('.nv-legendWrap')
+            .datum(data)
+            .call(legend);
+
+        if ( margin.top != legend.height()) {
+          margin.top = legend.height();
+          availableHeight = (height || parseInt(container.style('height')) || 400)
+                             - margin.top - margin.bottom;
+        }
+
+        wrap.select('.nv-legendWrap')
+            .attr('transform', 'translate(0,' + (-margin.top) +')');
+      }
+
+      //------------------------------------------------------------
+     
       wrap.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')'); 
+
+      var gradientWrap = g.select('.nv-gradientWrap')
+                                   .datum([data]);
 
       var xAxis = nv.models.axis()
                     .scale(x)
                     .ticks(10)
-                    .tickSize(-44)                          
+                    .tickSize(-44)
                     .orient("bottom");
 
-      gEnter.selectAll('rect').data(threshold.range().map(function(color) {
+      gradientWrap.selectAll('rect').data(threshold.range().map(function(color) {
          var d = threshold.invertExtent(color);
         
          if(d[0] == null) d[0] = x.domain()[0];
          if(d[1] == null) d[1] = x.domain()[1];
          return d;
       })).enter().append('rect')
-         .attr('height', availableHeight)
+         .attr('height', 45)
          .attr('x', function(d) { return x(d[0]); })
          .attr('width', function(d) { return x(d[1]) - x(d[0]); })
-         .style('fill', function(d) { return threshold(d[0]); })
-         .style('border-top', '1px #000 top');
+         .style('fill', function(d) { return threshold(d[0]); });
 
-      gEnter.append('g')
-           .attr('transform', 'translate(0,' + (availableWidth / 10) +  ')')                 
+      gradientWrap.append('g')
+           .attr('transform', 'translate(0,' + (availableWidth / 10) +  ')')
+           .attr('class', 'nv-gradientAxis')
            .call(xAxis);
 
-      gEnter.select('.tick').append('g')
-            .append('line')
-            .attr('x1',0)
-            .attr('x2', availableWidth+1)
-            .attr('y1',-45)
-            .attr('y2',-45);
+      var circles = gradientWrap.append('g');
 
-      //var dots = wrap.select()
-
-      gEnter.selectAll('.cpu')
+      circles.selectAll('circle')
            .data(data)
            .enter().append('circle')
-           .attr('class', 'cpu')
+           .attr('class', 'data-dot')
            .attr('r', 6)
-           .attr('cy', 35)
-           .attr('cx', availableWidth * (data[0]/100))
-           .style('fill','#ff6600')
+           .attr('cy', function(d,i) { return 10 + (25 * i) })
+           .attr('cx', function(d) { return availableWidth * (d.value/100) } )
+           .style('fill',function(d,i) { return color(d.key,i) })
            .style('stroke','black')
            .style('stroke-width','2px')
            .on("mouseover", function(d,i) {
              d3.select(this).classed('hover', true);
              dispatch.elementMouseover({               
-               label: 'CPU',
-               value: d,
-               point: d.data,
+               label: d.key,
+               value: d.value,
                pointIndex: i,
                pos: {left: d3.event.pageX, top: d3.event.pageY},
                id: id,
@@ -128,42 +157,6 @@ nv.models.sumoUtilization = function() {
              dispatch.elementMouseout({
                label: 'CPU',
                value: d,
-               point: d.data,
-               pointIndex: i,
-               pos: {left: d3.event.pageX, top: d3.event.pageY},
-               owningSVG: this.ownerSVGElement
-             });
-           });
-
-      gEnter.selectAll('.mem')
-           .data(data)
-           .enter().append('circle')
-           .attr('class', 'mem')
-           .attr('r', 6)
-           .attr('cy', 10)
-           .attr('cx', availableWidth * (data[1]/100))
-           .style('fill','#5787b7')
-           .style('stroke','black')
-           .style('stroke-width','2px')
-           .on("mouseover", function(d,i) {
-             d3.select(this).classed('hover', true);
-             dispatch.elementMouseover({               
-               label: 'Memory',
-               value: d,
-               point: d.data,
-               pointIndex: i,
-               pos: {left: d3.event.pageX, top: d3.event.pageY},
-               id: id,
-               owningSVG: this.ownerSVGElement
-             });
-             d3.event.stopPropagation();
-           })
-           .on('mouseout', function(d,i){
-             d3.select(this).classed('hover', false);
-             dispatch.elementMouseout({
-               label: 'Memory',
-               value: d,
-               point: d.data,
                pointIndex: i,
                pos: {left: d3.event.pageX, top: d3.event.pageY},
                owningSVG: this.ownerSVGElement
@@ -223,6 +216,17 @@ nv.models.sumoUtilization = function() {
      return chart;
   }
 
+  chart.color = function(_) {
+    if (!arguments.length) return color;
+    color = nv.utils.getColor(_);
+    return chart;
+  };
+
+  chart.label = function(_) {
+    if(!arguments.length) return getLabel;
+    getLabel = _;
+    return chart;
+  }
 
   //============================================================
 
