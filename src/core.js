7d7cdeb3ee7c8213c510dbd06253a0fcaf82fc37
Reece <RPegues@tripwire.com>
2014-11-23 11:03:35 -0500

    don't blow up on IE9 if nv.dev is on and dev tools are closed by also checking if console object exists at all.

--- src/core.js
+++ src/core.js
@@ -60,9 +60,9 @@ if (nv.dev) {
 // then calling Function.prototype.bind with with anything other than a function
 // causes a TypeError to be thrown.
 nv.log = function() {
-    if (nv.dev && console.log && console.log.apply)
+    if (nv.dev && window.console && console.log && console.log.apply)
         console.log.apply(console, arguments);
-    else if (nv.dev && typeof console.log == "function" && Function.prototype.bind) {
+    else if (nv.dev && window.console && typeof console.log == "function" && Function.prototype.bind) {
         var log = Function.prototype.bind.call(console.log, console);
         log.apply(console, arguments);
     }
