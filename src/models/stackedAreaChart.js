3c2cd269fab98c25bcb02e7a30760ad6037498ff
Jakub Dostal <github@dostal.co.uk>
2015-01-10 16:16:15 +0000

    Reverting the last commit to avoid confusion
    This will be submitted as a pull request once the multibar related
    stuff is resolved.

--- src/models/stackedAreaChart.js
+++ src/models/stackedAreaChart.js
@@ -31,6 +31,7 @@ nv.models.stackedAreaChart = function() {
         }
         , x //can be accessed via chart.xScale()
         , y //can be accessed via chart.yScale()
+        , yAxisTickFormat = d3.format(',.2f')
         , state = nv.utils.state()
         , defaultState = null
         , noData = 'No Data Available.'
@@ -275,7 +276,7 @@ nv.models.stackedAreaChart = function() {
                     .ticks(stacked.offset() == 'wiggle' ? 0 : nv.utils.calcTicksY(availableHeight/36, data) )
                     .tickSize(-availableWidth, 0)
                     .setTickFormat( (stacked.style() == 'expand' || stacked.style() == 'stack_percent')
-                        ? d3.format('%') : yAxis.tickFormat());
+                        ? d3.format('%') : yAxisTickFormat);
 
                 g.select('.nv-y.nv-axis')
                     .transition().duration(0)
@@ -481,6 +482,7 @@ nv.models.stackedAreaChart = function() {
         noData:    {get: function(){return noData;}, set: function(_){noData=_;}},
         showControls:    {get: function(){return showControls;}, set: function(_){showControls=_;}},
         controlLabels:    {get: function(){return controlLabels;}, set: function(_){controlLabels=_;}},
+        yAxisTickFormat:    {get: function(){return yAxisTickFormat;}, set: function(_){yAxisTickFormat=_;}},
 
         // options that require extra logic in the setter
         margin: {get: function(){return margin;}, set: function(_){
