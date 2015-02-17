240f5c2bb344f87c176ff563f39b7342ff04e084
liquidpele <reecepegues+github@gmail.com>
2015-02-16 23:53:10 -0500

    Manually apply https://github.com/novus/nvd3/pull/431 and fix corresponding CSS as well.

--- src/models/multiChart.js
+++ src/models/multiChart.js
@@ -105,9 +105,9 @@ nv.models.multiChart = function() {
             var wrap = container.selectAll('g.wrap.multiChart').data([data]);
             var gEnter = wrap.enter().append('g').attr('class', 'wrap nvd3 multiChart').append('g');
 
-            gEnter.append('g').attr('class', 'x axis');
-            gEnter.append('g').attr('class', 'y1 axis');
-            gEnter.append('g').attr('class', 'y2 axis');
+            gEnter.append('g').attr('class', 'nv-x nv-axis');
+            gEnter.append('g').attr('class', 'nv-y1 nv-axis');
+            gEnter.append('g').attr('class', 'nv-y2 nv-axis');
             gEnter.append('g').attr('class', 'lines1Wrap');
             gEnter.append('g').attr('class', 'lines2Wrap');
             gEnter.append('g').attr('class', 'bars1Wrap');
@@ -176,30 +176,17 @@ nv.models.multiChart = function() {
             g.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
             var lines1Wrap = g.select('.lines1Wrap')
-                .datum(
-                    dataLines1.filter(function(d){return !d.disabled})
-                );
+                .datum(dataLines1.filter(function(d){return !d.disabled}));
             var bars1Wrap = g.select('.bars1Wrap')
-                .datum(
-                    dataBars1.filter(function(d){return !d.disabled})
-                );
+                .datum(dataBars1.filter(function(d){return !d.disabled}));
             var stack1Wrap = g.select('.stack1Wrap')
-                .datum(
-                    dataStack1.filter(function(d){return !d.disabled})
-                );
-
+                .datum(dataStack1.filter(function(d){return !d.disabled}));
             var lines2Wrap = g.select('.lines2Wrap')
-                .datum(
-                    dataLines2.filter(function(d){return !d.disabled})
-                );
+                .datum(dataLines2.filter(function(d){return !d.disabled}));
             var bars2Wrap = g.select('.bars2Wrap')
-                .datum(
-                    dataBars2.filter(function(d){return !d.disabled})
-                );
+                .datum(dataBars2.filter(function(d){return !d.disabled}));
             var stack2Wrap = g.select('.stack2Wrap')
-                .datum(
-                    dataStack2.filter(function(d){return !d.disabled})
-                );
+                .datum(dataStack2.filter(function(d){return !d.disabled}));
 
             var extraValue1 = dataStack1.length ? dataStack1.map(function(a){return a.values}).reduce(function(a,b){
                 return a.map(function(aVal,i){return {x: aVal.x, y: aVal.y + b[i].y}})
@@ -235,9 +222,9 @@ nv.models.multiChart = function() {
                 .ticks( nv.utils.calcTicksX(availableWidth/100, data) )
                 .tickSize(-availableHeight, 0);
 
-            g.select('.x.axis')
+            g.select('.nv-x.nv-axis')
                 .attr('transform', 'translate(0,' + availableHeight + ')');
-            d3.transition(g.select('.x.axis'))
+            d3.transition(g.select('.nv-x.nv-axis'))
                 .call(xAxis);
 
             yAxis1
@@ -245,21 +232,21 @@ nv.models.multiChart = function() {
                 .tickSize( -availableWidth, 0);
 
 
-            d3.transition(g.select('.y1.axis'))
+            d3.transition(g.select('.nv-y1.nv-axis'))
                 .call(yAxis1);
 
             yAxis2
                 .ticks( nv.utils.calcTicksY(availableHeight/36, data) )
                 .tickSize( -availableWidth, 0);
 
-            d3.transition(g.select('.y2.axis'))
+            d3.transition(g.select('.nv-y2.nv-axis'))
                 .call(yAxis2);
 
-            g.select('.y1.axis')
+            g.select('.nv-y1.nv-axis')
                 .classed('nv-disabled', series1.length ? false : true)
                 .attr('transform', 'translate(' + x.range()[0] + ',0)');
 
-            g.select('.y2.axis')
+            g.select('.nv-y2.nv-axis')
                 .classed('nv-disabled', series2.length ? false : true)
                 .attr('transform', 'translate(' + x.range()[1] + ',0)');
 
