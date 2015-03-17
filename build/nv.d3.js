46e4af1827d3cf5f84990d0d775b6796a8473c5f
mgalloue <mathilde.gallouet@ansys.com>
2015-03-17 11:25:19 +0100

    Revert any files from the build/ directory.

--- build/nv.d3.js
+++ build/nv.d3.js
@@ -1,4 +1,4 @@
-/* nvd3 version 1.8.1 (https://github.com/novus/nvd3) 2015-03-10 */
+/* nvd3 version 1.8.1 (https://github.com/novus/nvd3) 2015-03-15 */
 (function(){
 
 // set up main nv object
@@ -2338,6 +2338,7 @@ nv.models.cumulativeLineChart = function() {
         , legend = nv.models.legend()
         , controls = nv.models.legend()
         , interactiveLayer = nv.interactiveGuideline()
+        , tooltip = nv.models.tooltip()
         ;
 
     var margin = {top: 30, right: 30, bottom: 50, left: 60}
@@ -2348,14 +2349,9 @@ nv.models.cumulativeLineChart = function() {
         , showXAxis = true
         , showYAxis = true
         , rightAlignYAxis = false
-        , tooltips = true
         , showControls = true
         , useInteractiveGuideline = false
         , rescaleY = true
-        , tooltip = function(key, x, y, e, graph) {
-            return '<h3>' + key + '</h3>' +
-                '<p>' +  y + ' at ' + x + '</p>'
-        }
         , x //can be accessed via chart.xScale()
         , y //can be accessed via chart.yScale()
         , id = lines.id()
@@ -2363,7 +2359,7 @@ nv.models.cumulativeLineChart = function() {
         , defaultState = null
         , noData = null
         , average = function(d) { return d.average }
-        , dispatch = d3.dispatch('tooltipShow', 'tooltipHide', 'stateChange', 'changeState', 'renderEnd')
+        , dispatch = d3.dispatch('stateChange', 'changeState', 'renderEnd')
         , transitionDuration = 250
         , duration = 250
         , noErrorCheck = false  //if set to TRUE, will bypass an error check in the indexify function.
@@ -2372,13 +2368,14 @@ nv.models.cumulativeLineChart = function() {
     state.index = 0;
     state.rescaleY = rescaleY;
 
-    xAxis
-        .orient('bottom')
-        .tickPadding(7)
-    ;
-    yAxis
-        .orient((rightAlignYAxis) ? 'right' : 'left')
-    ;
+    xAxis.orient('bottom').tickPadding(7);
+    yAxis.orient((rightAlignYAxis) ? 'right' : 'left');
+
+    tooltip.valueFormatter(function(d, i) {
+        return yAxis.tickFormat()(d, i);
+    }).headerFormatter(function(d, i) {
+        return xAxis.tickFormat()(d, i);
+    });
 
     controls.updateState(false);
 
@@ -2390,16 +2387,6 @@ nv.models.cumulativeLineChart = function() {
         , index = {i: 0, x: 0}
         , renderWatch = nv.utils.renderWatch(dispatch, duration)
         ;
-
-    var showTooltip = function(e, offsetElement) {
-        var left = e.pos[0] + ( offsetElement.offsetLeft || 0 ),
-            top = e.pos[1] + ( offsetElement.offsetTop || 0),
-            x = xAxis.tickFormat()(lines.x()(e.point, e.pointIndex)),
-            y = yAxis.tickFormat()(lines.y()(e.point, e.pointIndex)),
-            content = tooltip(e.series.key, x, y, e, chart);
-
-        nv.tooltip.show([left, top], content, null, null, offsetElement);
-    };
 
     var stateGetter = function(data) {
         return function(){
@@ -2812,7 +2799,6 @@ nv.models.cumulativeLineChart = function() {
                 interactiveLayer.tooltip
                     .position({left: pointXLocation + margin.left, top: e.mouseY + margin.top})
                     .chartContainer(that.parentNode)
-                    .enabled(tooltips)
                     .valueFormatter(function(d,i) {
                         return yAxis.tickFormat()(d);
                     })
@@ -2827,16 +2813,7 @@ nv.models.cumulativeLineChart = function() {
             });
 
             interactiveLayer.dispatch.on("elementMouseout",function(e) {
-                dispatch.tooltipHide();
                 lines.clearHighlights();
-            });
-
-            dispatch.on('tooltipShow', function(e) {
-                if (tooltips) showTooltip(e, that.parentNode);
-            });
-
-            dispatch.on('tooltipHide', function() {
-                if (tooltips) nv.tooltip.cleanup();
             });
 
             // Update chart from a state object passed to event handler
@@ -2877,13 +2854,18 @@ nv.models.cumulativeLineChart = function() {
     // Event Handling/Dispatching (out of chart's scope)
     //------------------------------------------------------------
 
-    lines.dispatch.on('elementMouseover.tooltip', function(e) {
-        e.pos = [e.pos[0] +  margin.left, e.pos[1] + margin.top];
-        dispatch.tooltipShow(e);
+    lines.dispatch.on('elementMouseover.tooltip', function(evt) {
+        var point = {
+            x: chart.x()(evt.point),
+            y: chart.y()(evt.point),
+            color: evt.point.color
+        };
+        evt.point = point;
+        tooltip.data(evt).position(evt.pos).hidden(false);
     });
 
-    lines.dispatch.on('elementMouseout.tooltip', function(e) {
-        dispatch.tooltipHide(e);
+    lines.dispatch.on('elementMouseout.tooltip', function(evt) {
+        tooltip.hidden(true)
     });
 
     //============================================================
@@ -2936,6 +2918,7 @@ nv.models.cumulativeLineChart = function() {
     chart.yAxis = yAxis;
     chart.interactiveLayer = interactiveLayer;
     chart.state = state;
+    chart.tooltip = tooltip;
 
     chart.options = nv.utils.optionsFunc.bind(chart);
 
@@ -2947,13 +2930,23 @@ nv.models.cumulativeLineChart = function() {
         showControls:     {get: function(){return showControls;}, set: function(_){showControls=_;}},
         showLegend: {get: function(){return showLegend;}, set: function(_){showLegend=_;}},
         average: {get: function(){return average;}, set: function(_){average=_;}},
-        tooltips:    {get: function(){return tooltips;}, set: function(_){tooltips=_;}},
-        tooltipContent:    {get: function(){return tooltip;}, set: function(_){tooltip=_;}},
         defaultState:    {get: function(){return defaultState;}, set: function(_){defaultState=_;}},
         noData:    {get: function(){return noData;}, set: function(_){noData=_;}},
         showXAxis:    {get: function(){return showXAxis;}, set: function(_){showXAxis=_;}},
         showYAxis:    {get: function(){return showYAxis;}, set: function(_){showYAxis=_;}},
         noErrorCheck:    {get: function(){return noErrorCheck;}, set: function(_){noErrorCheck=_;}},
+
+        // deprecated options
+        tooltips:    {get: function(){return tooltip.enabled();}, set: function(_){
+            // deprecated after 1.7.1
+            nv.deprecated('tooltips', 'use chart.tooltip.enabled() instead');
+            tooltip.enabled(!!_);
+        }},
+        tooltipContent:    {get: function(){return tooltip.contentGenerator();}, set: function(_){
+            // deprecated after 1.7.1
+            nv.deprecated('tooltipContent', 'use chart.tooltip.contentGenerator() instead');
+            tooltip.contentGenerator(_);
+        }},
 
         // options that require extra logic in the setter
         margin: {get: function(){return margin;}, set: function(_){
@@ -7963,7 +7956,6 @@ nv.models.multiChart = function() {
 
             function mouseover_stack(evt) {
                 var yaxis = data[evt.seriesIndex].yAxis === 2 ? yAxis2 : yAxis1;
-                var pos = {left: evt.pos[0] +  margin.left, top: evt.pos[1] + margin.top};
                 evt.point['x'] = evt.point['x'] || evt.point[0];
                 evt.point['y'] = evt.point['y'] || evt.point[1];
                 tooltip
@@ -7972,7 +7964,7 @@ nv.models.multiChart = function() {
                         return yaxis.tickFormat()(d, i);
                     })
                     .data(evt)
-                    .position(pos)
+                    .position(evt.pos)
                     .hidden(false);
             }
 
@@ -8700,8 +8692,8 @@ nv.models.pie = function() {
                 });
             });
 
-            slices.attr('fill', function(d,i) { return color(d, i); });
-            slices.attr('stroke', function(d,i) { return color(d, i); });
+            slices.attr('fill', function(d,i) { return color(d.data, i); });
+            slices.attr('stroke', function(d,i) { return color(d.data, i); });
 
             var paths = ae.append('path').each(function(d) {
                 this._current = d;
@@ -9040,19 +9032,20 @@ nv.models.pieChart = function() {
                     wrap.select('.nv-legendWrap')
                         .attr('transform', 'translate(0,' + (-margin.top) +')');
                 } else if (legendPosition === "right") {
-                    legend.height(availableHeight).width(availableWidth - availableHeight).key(pie.x());
-
-                    wrap.select('.nv-legendWrap')
-                        .datum(data)
-                        .call(legend);
+                    legend.height( availableHeight ).key(pie.x());
 
                     if ( margin.right != legend.width()) {
+                        if (legend.width() > availableWidth / 2) {
+                            legend.width(availableWidth / 2);
+                            availableWidth -= legend.width();
+                        }
                         margin.right = legend.width();
-                        availableWidth = nv.utils.availableWidth(width, container, margin);
                     }
 
                     wrap.select('.nv-legendWrap')
-                        .attr('transform', 'translate(' + (margin.left + availableHeight) +',0)');
+                        .datum(data)
+                        .call(legend)
+                        .attr('transform', 'translate(' + (availableWidth) +',0)');
                 }
             }
             wrap.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
@@ -9119,6 +9112,7 @@ nv.models.pieChart = function() {
     chart.legend = legend;
     chart.dispatch = dispatch;
     chart.pie = pie;
+    chart.tooltip = tooltip;
     chart.options = nv.utils.optionsFunc.bind(chart);
 
     // use Object get/set functionality to map between vars and chart functions
@@ -9321,8 +9315,8 @@ nv.models.scatter = function() {
                                 var pX = getX(point,pointIndex);
                                 var pY = getY(point,pointIndex);
 
-                                return [x(pX)+ Math.random() * 1e-7,
-                                        y(pY)+ Math.random() * 1e-7,
+                                return [x(pX)+ Math.random() * 1e-4,
+                                        y(pY)+ Math.random() * 1e-4,
                                     groupIndex,
                                     pointIndex, point]; //temp hack to add noise until I think of a better way so there are no duplicates
                             })
