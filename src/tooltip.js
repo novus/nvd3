5cd4eb8d7efe2ee9f6d9ee30e4184b01dd8e9957
Tim Wu <tim.github@admorphit.com>
2015-03-18 11:33:01 -0700

    change property offset to tooltipOffset

--- src/tooltip.js
+++ src/tooltip.js
@@ -40,7 +40,7 @@
             ,   tooltip = null // d3 select of tooltipElem below
             ,   tooltipElem = null  //actual DOM element representing the tooltip.
             ,   position = {left: null, top: null}   //Relative position of the tooltip inside chartContainer.
-            ,   offset = {left: 0, top: 0}   //Offset of tooltip against the pointer
+            ,   tooltipOffset = {left: 0, top: 0}   //Offset of tooltip against the pointer
             ,   enabled = true  //True -> tooltips are rendered. False -> don't render tooltips.
             ,   duration = 100 // duration for tooltip movement
             ,   headerEnabled = true
@@ -226,8 +226,8 @@
                 }
                 
                 // adjust tooltip offsets
-                left -= offset.left;
-                top -= offset.top;
+                left -= tooltipOffset.left;
+                top -= tooltipOffset.top;
 
                 // using tooltip.style('transform') returns values un-usable for tween
                 var box = tooltipElem.getBoundingClientRect();
@@ -384,9 +384,9 @@
                 position.left = _.left !== undefined ? _.left : position.left;
                 position.top  = _.top  !== undefined ? _.top  : position.top;
             }},
-            offset: {get: function(){return offset;}, set: function(_){
-                offset.left = _.left !== undefined ? _.left : offset.left;
-                offset.top  = _.top  !== undefined ? _.top  : offset.top;
+            tooltipOffset: {get: function(){return tooltipOffset;}, set: function(_){
+                tooltipOffset.left = _.left !== undefined ? _.left : tooltipOffset.left;
+                tooltipOffset.top  = _.top  !== undefined ? _.top  : tooltipOffset.top;
             }},
             hidden: {get: function(){return hidden;}, set: function(_){
                 if (hidden != _) {
