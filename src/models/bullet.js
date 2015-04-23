915440bc4204c23576bd56d32afb7d6b2ea5ac56
Jason Draper <jason@drapergeek.com>
2015-04-23 11:03:19 -0400

    Just cleaning up a bit

--- src/models/bullet.js
+++ src/models/bullet.js
@@ -120,32 +120,29 @@ nv.models.bullet = function() {
                 });
 
             var h3 =  availableHeight / 6;
-            if (markerz.length > 0) {
-              var length = markerz.length;
-              console.log(length);
-              for (var index = 0; index < length; ++index) {
-                gEnter.append('path').attr('class', 'nv-markerTriangle').attr("data-marker", markerz[index]);
-                g.select('[data-marker="'+ markerz[index] +'"]')
-                    .attr('transform', function(d) { return 'translate(' + x1(markerz[index]) + ',' + (availableHeight / 2) + ')' })
-                    .attr('d', 'M0,' + h3 + 'L' + h3 + ',' + (-h3) + ' ' + (-h3) + ',' + (-h3) + 'Z')
-                    .on('mouseover', function() {
-                        dispatch.elementMouseover({
-                            value: markerz[index],
-                            label: markerLabelz[0] || 'Previous',
-                            pos: [x1(markerz[index]), availableHeight/2]
-                        })
-                    })
-                    .on('mouseout', function() {
-                        dispatch.elementMouseout({
-                            value: markerz[index],
-                            label: markerLabelz[0] || 'Previous'
-                        })
-                    });
-              }
-            } else {
-                g.selectAll('path.nv-markerTriangle').remove();
+            var length = markerz.length;
+            for (var index = 0; index < length; ++index) {
+              gEnter
+                .append('path')
+                .attr('class', 'nv-markerTriangle')
+                .attr("data-marker", markerz[index]);
+              g.select('[data-marker="'+ markerz[index] +'"]')
+                  .attr('transform', function(d) { return 'translate(' + x1(markerz[index]) + ',' + (availableHeight / 2) + ')' })
+                  .attr('d', 'M0,' + h3 + 'L' + h3 + ',' + (-h3) + ' ' + (-h3) + ',' + (-h3) + 'Z')
+                  .on('mouseover', function() {
+                      dispatch.elementMouseover({
+                          value: markerz[index],
+                          label: markerLabelz[0] || 'Previous',
+                          pos: [x1(markerz[index]), availableHeight/2]
+                      })
+                  })
+                  .on('mouseout', function() {
+                      dispatch.elementMouseout({
+                          value: markerz[index],
+                          label: markerLabelz[0] || 'Previous'
+                      })
+                  });
             }
-
             wrap.selectAll('.nv-range')
                 .on('mouseover', function(d,i) {
                     var label = rangeLabelz[i] || (!i ? "Maximum" : i == 1 ? "Mean" : "Minimum");
