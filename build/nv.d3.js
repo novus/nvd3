1e1aa162e21b869af745eef2a325e1ab7cfde23e
Jason Draper <jason@drapergeek.com>
2015-04-23 14:00:09 -0400

    Clean up the code a bit

--- build/nv.d3.js
+++ build/nv.d3.js
@@ -1933,39 +1933,33 @@ nv.models.bullet = function() {
 
             var h3 =  availableHeight / 6;
             var length = markerz.length;
-            // for (var index = 0; index < length; ++index) {
-            //   console.log(index);
-            //   var marker = markerz[index];
-            //   var label = markerLabelz[index];
-            var things = [
-            ]
-            for (var index = 0; index < length; ++index) {
-              things.push({value: markerz[index], label: markerLabelz[index]});
-            }
+            var markerData = markerz.map (function(marker, index) {
+                return {value: marker, label: markerLabelz[index]}
+            });
 
-              gEnter
-                .selectAll("path.nv-markerTriangle")
-                .data(things)
-                .enter()
-                .append('path')
-                .attr('class', 'nv-markerTriangle')
-                .attr('transform', function(d) { return 'translate(' + x1(d.value) + ',' + (availableHeight / 2) + ')' })
-                .attr('d', 'M0,' + h3 + 'L' + h3 + ',' + (-h3) + ' ' + (-h3) + ',' + (-h3) + 'Z')
-                .on('mouseover', function(d, i) {
-                  dispatch.elementMouseover({
-                    value: d.value,
-                    label: d.label || 'Previous',
-                    pos: [x1(d.value), availableHeight/2]
-                  })
-
+            gEnter
+              .selectAll("path.nv-markerTriangle")
+              .data(markerData)
+              .enter()
+              .append('path')
+              .attr('class', 'nv-markerTriangle')
+              .attr('transform', function(d) { return 'translate(' + x1(d.value) + ',' + (availableHeight / 2) + ')' })
+              .attr('d', 'M0,' + h3 + 'L' + h3 + ',' + (-h3) + ' ' + (-h3) + ',' + (-h3) + 'Z')
+              .on('mouseover', function(d, i) {
+                dispatch.elementMouseover({
+                  value: d.value,
+                  label: d.label || 'Previous',
+                  pos: [x1(d.value), availableHeight/2]
                 })
-                .on('mouseout', function(d, i) {
-                    dispatch.elementMouseout({
-                        value: d.value,
-                        label: d.label || 'Previous'
-                    })
-                });
-            //}
+
+              })
+              .on('mouseout', function(d, i) {
+                  dispatch.elementMouseout({
+                      value: d.value,
+                      label: d.label || 'Previous'
+                  })
+              });
+
             wrap.selectAll('.nv-range')
                 .on('mouseover', function(d,i) {
                     var label = rangeLabelz[i] || (!i ? "Maximum" : i == 1 ? "Mean" : "Minimum");
