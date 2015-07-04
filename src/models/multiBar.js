96d53219bfa4de918013a0870d79267e78ac18d8
WalterDa <walterdangalante@gmail.com>
2015-07-03 19:31:19 -0700

    Fix proposal for #1121
    Attempting to fix #1121. This fixes the issue in my case, not sure about side effects of this fix.

--- src/models/multiBar.js
+++ src/models/multiBar.js
@@ -352,9 +352,9 @@ nv.models.multiBar = function() {
                     .select('rect')
                     .attr('height', function(d,i,j) {
                         if (!data[j].nonStackable) {
-                            return Math.max(Math.abs(y(d.y+d.y0) - y(d.y0)), 1);
+                            return Math.max(Math.abs(y(d.y+d.y0) - y(d.y0)), 0);
                         } else {
-                            return Math.max(Math.abs(y(getY(d,i)) - y(0)),1) || 0;
+                            return Math.max(Math.abs(y(getY(d,i)) - y(0)), 0) || 0;
                         }
                     })
                     .attr('width', function(d,i,j){
