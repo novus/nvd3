a8980271f5f64db9d5622ce46262d9129cd9f269
Mihaly Alberti <ksgy@x-plane.hu>
2016-07-13 17:48:41 +0100

    Refactor

--- src/models/sankey.js
+++ src/models/sankey.js
@@ -1,219 +1,5 @@
-
-if (typeof JSON.decycle !== 'function') {
-    (function(){
-
-        /**
-         * Allows stringifing DOM elements.
-         *
-         * This is done in hope to identify the node when dumping.
-         *
-         * @param {Element} node DOM Node (works best for DOM Elements).
-         * @returns {String}
-         */
-        function stringifyNode(node) {
-            var text = "";
-            switch (node.nodeType) {
-                case node.ELEMENT_NODE:
-                    text = node.nodeName.toLowerCase();
-                    if (node.id.length) {
-                        text += '#' + node.id;
-                    }
-                    else {
-                        if (node.className.length) {
-                            text += '.' + node.className.replace(/ /, '.');
-                        }
-                        if ('textContent' in node) {
-                            text += '{textContent:'
-                                + (node.textContent.length < 20 ? node.textContent : node.textContent.substr(0, 20) + '...')
-                                + '}'
-                            ;
-                        }
-                    }
-                    break;
-                // info on values: http://www.w3.org/TR/DOM-Level-2-Core/core.html#ID-1841493061
-                default:
-                    text = node.nodeName;
-                    if (node.nodeValue !== null) {
-                        text += '{value:'
-                            + (node.nodeValue.length < 20 ? node.nodeValue : node.nodeValue.substr(0, 20) + '...')
-                            + '}'
-                        ;
-                    }
-                    break;
-            }
-            return text;
-        }
-
-        JSON.decycle = function decycle(object, stringifyNodes) {
-            'use strict';
-
-// Make a deep copy of an object or array, assuring that there is at most
-// one instance of each object or array in the resulting structure. The
-// duplicate references (which might be forming cycles) are replaced with
-// an object of the form
-//      {$ref: PATH}
-// where the PATH is a JSONPath string that locates the first occurance.
-// So,
-//      var a = [];
-//      a[0] = a;
-//      return JSON.stringify(JSON.decycle(a));
-// produces the string '[{"$ref":"$"}]'.
-
-// NOTE! If your object contains DOM Nodes you might want to use `stringifyNodes` option
-// This will dump e.g. `div` with id="some-id" to string: `div#some-id`.
-// You will avoid some problems, but you won't to be able to fully retro-cycle.
-// To dump almost any variable use: `alert(JSON.stringify(JSON.decycle(variable, true)));`
-
-// JSONPath is used to locate the unique object. $ indicates the top level of
-// the object or array. [NUMBER] or [STRING] indicates a child member or
-// property.
-
-            var objects = [],   // Keep a reference to each unique object or array
-                stringifyNodes = typeof(stringifyNodes) === 'undefined' ? false : stringifyNodes,
-                paths = [];     // Keep the path to each unique object or array
-
-            return (function derez(value, path) {
-
-// The derez recurses through the object, producing the deep copy.
-
-                var i,          // The loop counter
-                    name,       // Property name
-                    nu;         // The new object or array
-
-// if we have a DOM Element/Node convert it to textual info.
-
-                if (stringifyNodes && typeof value === 'object' && value !== null && 'nodeType' in value) {
-                    return stringifyNode(value);
-                }
-
-// typeof null === 'object', so go on if this value is really an object but not
-// one of the weird builtin objects.
-
-                if (typeof value === 'object' && value !== null &&
-                    !(value instanceof Boolean) &&
-                    !(value instanceof Date)    &&
-                    !(value instanceof Number)  &&
-                    !(value instanceof RegExp)  &&
-                    !(value instanceof String)) {
-
-// If the value is an object or array, look to see if we have already
-// encountered it. If so, return a $ref/path object. This is a hard way,
-// linear search that will get slower as the number of unique objects grows.
-
-                    for (i = 0; i < objects.length; i += 1) {
-                        if (objects[i] === value) {
-                            return {$ref: paths[i]};
-                        }
-                    }
-
-// Otherwise, accumulate the unique value and its path.
-
-                    objects.push(value);
-                    paths.push(path);
-
-// If it is an array, replicate the array.
-
-                    if (Object.prototype.toString.apply(value) === '[object Array]') {
-                        nu = [];
-                        for (i = 0; i < value.length; i += 1) {
-                            nu[i] = derez(value[i], path + '[' + i + ']');
-                        }
-                    } else {
-
-// If it is an object, replicate the object.
-
-                        nu = {};
-                        for (name in value) {
-                            if (Object.prototype.hasOwnProperty.call(value, name)) {
-                                nu[name] = derez(value[name],
-                                    path + '[' + JSON.stringify(name) + ']');
-                            }
-                        }
-                    }
-                    return nu;
-                }
-                return value;
-            }(object, '$'));
-        };
-    })();
-}
-
-
-if (typeof JSON.retrocycle !== 'function') {
-    JSON.retrocycle = function retrocycle($) {
-        'use strict';
-
-// Restore an object that was reduced by decycle. Members whose values are
-// objects of the form
-//      {$ref: PATH}
-// are replaced with references to the value found by the PATH. This will
-// restore cycles. The object will be mutated.
-
-// The eval function is used to locate the values described by a PATH. The
-// root object is kept in a $ variable. A regular expression is used to
-// assure that the PATH is extremely well formed. The regexp contains nested
-// * quantifiers. That has been known to have extremely bad performance
-// problems on some browsers for very long strings. A PATH is expected to be
-// reasonably short. A PATH is allowed to belong to a very restricted subset of
-// Goessner's JSONPath.
-
-// So,
-//      var s = '[{"$ref":"$"}]';
-//      return JSON.retrocycle(JSON.parse(s));
-// produces an array containing a single element which is the array itself.
-
-        var px =
-            /^\$(?:\[(?:\d+|\"(?:[^\\\"\u0000-\u001f]|\\([\\\"\/bfnrt]|u[0-9a-zA-Z]{4}))*\")\])*$/;
-
-        (function rez(value) {
-
-// The rez function walks recursively through the object looking for $ref
-// properties. When it finds one that has a value that is a path, then it
-// replaces the $ref object with a reference to the value that is found by
-// the path.
-
-            var i, item, name, path;
-
-            if (value && typeof value === 'object') {
-                if (Object.prototype.toString.apply(value) === '[object Array]') {
-                    for (i = 0; i < value.length; i += 1) {
-                        item = value[i];
-                        if (item && typeof item === 'object') {
-                            path = item.$ref;
-                            if (typeof path === 'string' && px.test(path)) {
-                                value[i] = eval(path);
-                            } else {
-                                rez(item);
-                            }
-                        }
-                    }
-                } else {
-                    for (name in value) {
-                        if (typeof value[name] === 'object') {
-                            item = value[name];
-                            if (item) {
-                                path = item.$ref;
-                                if (typeof path === 'string' && px.test(path)) {
-                                    value[name] = eval(path);
-                                } else {
-                                    rez(item);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }($));
-        return $;
-    };
-}
-
-
-
-
-
 nv.models.sankey = function() {
-    "use strict";
+    'use strict';
 
     //============================================================
     // Public Variables with Default Settings
@@ -279,7 +65,7 @@ nv.models.sankey = function() {
         return sankey;
     };
 
-    // SVG path data generator, to be used as "d" attribute on "path" element selection.
+    // SVG path data generator, to be used as 'd' attribute on 'path' element selection.
     sankey.link = function() {
         var curvature = .5;
 
@@ -292,11 +78,10 @@ nv.models.sankey = function() {
                 x3 = xi(1 - curvature),
                 y0 = d.source.y + d.sy + d.dy / 2,
                 y1 = d.target.y + d.ty + d.dy / 2;
-            var linkPath = "M" + x0 + "," + y0
-                + "C" + x2 + "," + y0
-                + " " + x3 + "," + y1
-                + " " + x1 + "," + y1;
-            console.log('linkPath', linkPath);
+            var linkPath = 'M' + x0 + ',' + y0
+                + 'C' + x2 + ',' + y0
+                + ' ' + x3 + ',' + y1
+                + ' ' + x1 + ',' + y1;
             return linkPath;
         }
 
@@ -308,6 +93,10 @@ nv.models.sankey = function() {
 
         return link;
     };
+
+    //============================================================
+    // Private Variables
+    //------------------------------------------------------------
 
     // Populate the sourceLinks and targetLinks for each node.
     // Also, if the source and target are not objects, assume they are indices.
@@ -322,8 +111,8 @@ nv.models.sankey = function() {
         links.forEach(function(link) {
             var source = link.source,
                 target = link.target;
-            if (typeof source === "number") source = link.source = nodes[link.source];
-            if (typeof target === "number") target = link.target = nodes[link.target];
+            if (typeof source === 'number') source = link.source = nodes[link.source];
+            if (typeof target === 'number') target = link.target = nodes[link.target];
             source.sourceLinks.push(link);
             target.targetLinks.push(link);
         });
@@ -538,6 +327,7 @@ nv.models.sankey = function() {
 
     // Y-position of the middle of a node.
     function center(node) {
+        // TODO expose as a function?
         // return node.y + node.dy / 2;
         return node.dy;
     }
--- src/models/sankeyChart.js
+++ src/models/sankeyChart.js
@@ -14,19 +14,42 @@ nv.models.sankeyChart = function() {
         , nodeWidth = 36
         , nodePadding =  40
         , units =  'units'
-        , dispatch = d3.dispatch('legendClick', 'legendDblclick', 'legendMouseover', 'legendMouseout', 'stateChange')
         ;
+    //============================================================
+
+
+    //============================================================
+    // Private Variables
+    //------------------------------------------------------------
+
+    var formatNumber = d3.format(',.0f');    // zero decimal places
+    var format = function(d) {
+        return formatNumber(d) + ' ' + units;
+    };
+    var color = d3.scale.category20();
+    var linkTitle = function(d){
+        return d.source.name + ' → ' + d.target.name + '\n' + format(d.value);
+    };
+    var nodeFillColor = function(d){
+        return d.color = color(d.name.replace(/ .*/, ''));
+    };
+    var nodeStrokeColor = function(d){
+        return d3.rgb(d.color).darker(2);
+    };
+    var nodeTitle = function(d){
+        return d.name + '\n' + format(d.value);
+    };
+
+
+    //============================================================
 
     function chart(selection) {
         selection.each(function(data) {
-            var availableWidth = width - margin.left - margin.right;
-            var container = d3.select(this);
+            // TODO apply chart for each "selection" element
+
+            // var availableWidth = width - margin.left - margin.right;
+            // var container = d3.select(this);
             // nv.utils.initSVG(container);
-
-            var formatNumber = d3.format(',.0f');    // zero decimal places
-            var format = function(d) { return formatNumber(d) + ' ' + units; };
-            var color = d3.scale.category20();
-
 
             // append the svg canvas to the page
             var svg = d3.select('#sankey-chart').append('svg')
@@ -44,8 +67,8 @@ nv.models.sankeyChart = function() {
 
             var path = sankey.link();
 
-            var data = {"links":[{"source":0,"target":1,"value":2295},{"source":0,"target":5,"value":1199},{"source":1,"target":2,"value":1119},{"source":1,"target":5,"value":1176},{"source":2,"target":3,"value":487},{"source":2,"target":5,"value":632},{"source":3,"target":4,"value":301},{"source":3,"target":5,"value":186}],"nodes":[{"node":1,"name":"Landed on main page","source":null,"target":null},{"node":2,"name":"Homepage search","source":null,"target":null},{"node":3,"name":"Restaurant-meal checked","source":null,"target":null},{"node":4,"name":"Items added to basket","source":null,"target":null},{"node":5,"name":"Orders made","source":null,"target":null},{"node":6,"name":"Left","source":null,"target":null}]};
-
+            // TODO data from outside
+            // TODO error handling if data is invalid
 
             sankey
                 .nodes(data.nodes)
@@ -63,49 +86,43 @@ nv.models.sankeyChart = function() {
 
             // add the link titles
             link.append('title')
-                .text(function(d) {
-                    // TODO move "→" as an option
-                return d.source.name + ' → ' +
-                    d.target.name + '\n' + format(d.value); });
+                .text(linkTitle);
 
             // add in the nodes
             var node = svg.append('g').selectAll('.node')
                 .data(data.nodes)
                 .enter().append('g')
                 .attr('class', 'node')
-                .attr('transform', function(d) {
-                return 'translate(' + d.x + ',' + d.y + ')'; })
-            .call(d3.behavior.drag()
-                .origin(function(d) { return d; })
-            .on('dragstart', function() {
-                this.parentNode.appendChild(this); })
-            .on('drag', dragmove));
+                .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; })
+                .call(
+                    d3.behavior
+                        .drag()
+                        .origin(function(d) { return d; })
+                        .on('dragstart', function() {
+                            this.parentNode.appendChild(this);
+                        })
+                        .on('drag', dragmove)
+                );
 
             // add the rectangles for the nodes
             node.append('rect')
                 .attr('height', function(d) { return d.dy; })
-            .attr('width', sankey.nodeWidth())
-                .style('fill', function(d) {
-                    // TODO color as an optional fn
-                return d.color = color(d.name.replace(/ .*/, '')); })
-            .style('stroke', function(d) {
-                // TODO stroke color as an optional fn
-                return d3.rgb(d.color).darker(2); })
-            .append('title')
-                .text(function(d) {
-                    // TODO title as an optional fn
-                return d.name + '\n' + format(d.value); });
+                .attr('width', sankey.nodeWidth())
+                .style('fill', nodeFillColor)
+                .style('stroke', nodeStrokeColor)
+                .append('title')
+                .text(nodeTitle);
 
             // add in the title for the nodes
             node.append('text')
                 .attr('x', -6)
                 .attr('y', function(d) { return d.dy / 2; })
-            .attr('dy', '.35em')
+                .attr('dy', '.35em')
                 .attr('text-anchor', 'end')
                 .attr('transform', null)
                 .text(function(d) { return d.name; })
-            .filter(function(d) { return d.x < width / 2; })
-            .attr('x', 6 + sankey.nodeWidth())
+                .filter(function(d) { return d.x < width / 2; })
+                .attr('x', 6 + sankey.nodeWidth())
                 .attr('text-anchor', 'start');
 
             // the function for moving the nodes
@@ -114,12 +131,10 @@ nv.models.sankeyChart = function() {
                 'translate(' + d.x + ',' + (
                     d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))
                 ) + ')');
-            sankey.relayout();
-            link.attr('d', path);
-        }
-
+                sankey.relayout();
+                link.attr('d', path);
+            }
         });
-
 
         return chart;
     }
@@ -128,14 +143,15 @@ nv.models.sankeyChart = function() {
     // Expose Public Variables
     //------------------------------------------------------------
 
-    chart.dispatch = dispatch;
     chart.options = nv.utils.optionsFunc.bind(chart);
 
     chart._options = Object.create({}, {
         // simple options, just get/set the necessary values
-        units:          {get: function(){return units;}, set: function(_){units=_;}},
-        width:          {get: function(){return width;}, set: function(_){width=_;}},
-        height:         {get: function(){return height;}, set: function(_){height=_;}},
+        units:           {get: function(){return units;},     set: function(_){units=_;}},
+        width:           {get: function(){return width;},     set: function(_){width=_;}},
+        height:          {get: function(){return height;},    set: function(_){height=_;}},
+        format:          {get: function(){return format;},    set: function(_){format=_;}},
+        linkTitle:       {get: function(){return linkTitle;}, set: function(_){linkTitle=_;}},
 
         // options that require extra logic in the setter
         margin: {get: function(){return margin;}, set: function(_){
@@ -143,7 +159,13 @@ nv.models.sankeyChart = function() {
             margin.right  = _.right  !== undefined ? _.right  : margin.right;
             margin.bottom = _.bottom !== undefined ? _.bottom : margin.bottom;
             margin.left   = _.left   !== undefined ? _.left   : margin.left;
+        }},
+        nodeStyle: {get: function(){return {};}, set: function(_){
+            nodeFillColor   = _.fillColor   !== undefined ? _.fillColor   : nodeFillColor;
+            nodeStrokeColor = _.strokeColor !== undefined ? _.strokeColor : nodeStrokeColor;
+            nodeTitle       = _.title       !== undefined ? _.title       : nodeTitle;
         }}
+
     });
 
     nv.utils.initOptions(chart);
