ec72db87801881a33ed12bd4bdcb62033760d543
kib357 <kib357@outlook.com>
2016-07-28 18:59:49 +0500

    Update multiBarChart.js

--- src/models/multiBarChart.js
+++ src/models/multiBarChart.js
@@ -61,6 +61,14 @@ nv.models.multiBarChart = function() {
         .headerFormatter(function(d, i) {
             return xAxis.tickFormat()(d, i);
         });
+        
+    interactiveLayer.tooltip
+        .valueFormatter(function(d, i) {
+            return d == null ? "N/A" : yAxis.tickFormat()(d, i);
+        })
+        .headerFormatter(function(d, i) {
+            return xAxis.tickFormat()(d, i);
+        });
 
     controls.updateState(false);
 
