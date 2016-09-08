39717b76fd07a1f18ae36ae803d21b2c17611f75
arfsb <silverback@gmail.com>
2016-09-08 11:36:16 -0700

    fix: Line chart interactive guideline error when extent is not in ascending order
    Fixes a “TypeError: Cannot read property 'x' of undefined” error when
    using the interactive guideline for line charts if the extent is not in
    ascending order.
    
    In our use case, we reversed the x-axis on the line chart by reversing
    the x domain.  This caused series.values.filter to filter out all our
    values and result in the TypeError when calling interactiveBisect  A
    check was added to ensure the extent is in ascending order.

--- src/models/lineChart.js
+++ src/models/lineChart.js
@@ -285,6 +285,9 @@ nv.models.lineChart = function() {
                     })
                     .forEach(function(series,i) {
                         var extent = focusEnable ? (focus.brush.empty() ? focus.xScale().domain() : focus.brush.extent()) : x.domain();
+                        if (extent[0] > extent[1])
+                            extent.reverse();
+
                         var currentValues = series.values.filter(function(d,i) {
                             return lines.x()(d,i) >= extent[0] && lines.x()(d,i) <= extent[1];
                         });
