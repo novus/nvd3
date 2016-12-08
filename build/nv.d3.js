a3993a82441c75532bb68f57f07390b2c7385cab
liquidpele <reecepegues+github@gmail.com>
2016-12-08 07:54:44 -0500

    Build latest again to trigger travisCI to run and pass.  It crashed and marked build as failed -_-

--- build/nv.d3.js
+++ build/nv.d3.js
@@ -1,4 +1,4 @@
-/* nvd3 version 1.8.5-dev (https://github.com/novus/nvd3) 2016-12-01 */
+/* nvd3 version 1.8.5-dev (https://github.com/novus/nvd3) 2016-12-08 */
 (function(){
 
 // set up main nv object
