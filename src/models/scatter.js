5ea1fe949e8c61f1835eb06b67efccfb1f17bf88
KidA78 <paul@traackr.com>
2017-01-14 11:55:14 -0800

    Fixes novus/nvd3 #330 deduping voronoi vertices

--- src/models/scatter.js
+++ src/models/scatter.js
@@ -227,10 +227,10 @@ nv.models.scatter = function() {
                                     var pX = getX(point,pointIndex);
                                     var pY = getY(point,pointIndex);
 
-                                    return [nv.utils.NaNtoZero(x(pX))+ Math.random() * 1e-4,
-                                            nv.utils.NaNtoZero(y(pY))+ Math.random() * 1e-4,
+                                    return [nv.utils.NaNtoZero(x(pX)),
+                                            nv.utils.NaNtoZero(y(pY)),
                                         groupIndex,
-                                        pointIndex, point]; //temp hack to add noise until I think of a better way so there are no duplicates
+                                        pointIndex, point];
                                 })
                                 .filter(function(pointArray, pointIndex) {
                                     return pointActive(pointArray[4], pointIndex); // Issue #237.. move filter to after map, so pointIndex is correct!
@@ -255,6 +255,18 @@ nv.models.scatter = function() {
                         [width + 10,height + 10],
                         [width + 10,-10]
                     ]);
+
+                    // delete duplicates from vertices - essential assumption for d3.geom.voronoi
+                    var epsilon = 1e-6; // d3 uses 1e-6 to determine equivalence.
+                    vertices = vertices.sort(function(a,b){return ((a[0] - b[0]) || (a[1] - b[1]))});
+                    for (var i = 0; i < vertices.length - 1; ) {
+                        if ((Math.abs(vertices[i][0] - vertices[i+1][0]) < epsilon) &&
+                        (Math.abs(vertices[i][1] - vertices[i+1][1]) < epsilon)) {
+                            vertices.splice(i+1, 1);
+                        } else {
+                            i++;
+                        }
+                    }
 
                     var voronoi = d3.geom.voronoi(vertices).map(function(d, i) {
                         return {
