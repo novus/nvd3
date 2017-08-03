a6a5d8c3a9ab57fc67751e4d31e3d699e365cae2
Constantino Schillebeeckx <titous@gmail.com>
2017-08-03 14:58:34 -0700

    bug fix when switching between squash and non-squashed

--- src/models/distroPlot.js
+++ src/models/distroPlot.js
@@ -251,6 +251,8 @@ nv.models.distroPlot = function() {
             return reformat;
         }
 
+        console.log(colorGroup)
+
         // TODO not DRY
         // couldn't find a conditional way of doing the key() grouping
         var formatted;
@@ -265,6 +267,7 @@ nv.models.distroPlot = function() {
                 })
                 .entries(dat);
         } else {
+            allColorGroups = d3.set() // reset
             var tmp = d3.nest()
                 .key(function(d) { return getX(d); })
                 .key(function(d) { return colorGroup(d); })
